b9069fc101340724f54971d581e8c8e1c330c3af
lihongfei <lihongfei1@xiaomi.com>
2018-10-17 17:19:17 +0800

    fix package bug and add log4j

--- WebService/src/main/java/com/hongtu/slice/controller/ImageAnalysisController.java
+++ WebService/src/main/java/com/hongtu/slice/controller/ImageAnalysisController.java
@@ -1,6 +1,8 @@
 package com.hongtu.slice.controller;
 
 import com.hongtu.slice.util.Add;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ImageAnalysisController {
+    private static Logger LOGGER = LoggerFactory.getLogger(ImageAnalysisController.class);
+
     @ResponseBody
-    @RequestMapping(value="/add",method = RequestMethod.POST)
-    String add(@RequestBody Add param){
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    String add(@RequestBody Add param) {
         Double a = Double.valueOf(param.getA());
         Double b = Double.valueOf(param.getB());
-        return String.format("%f",a+b);
+        LOGGER.info("requst add service: a:{},b:{}",a,b);
+        return String.format("%f", a + b);
     }
 }
