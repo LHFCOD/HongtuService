67689ed88417d9f37dbf701e9fbbe4ee9efbe8c8
lihongfei <lihongfei1@xiaomi.com>
2018-10-20 16:44:07 +0800

    modify add function

--- WebService/src/main/java/com/hongtu/slice/controller/ImageAnalysisController.java
+++ WebService/src/main/java/com/hongtu/slice/controller/ImageAnalysisController.java
@@ -1,23 +1,20 @@
 package com.hongtu.slice.controller;
 
+import com.google.common.base.Throwables;
 import com.hongtu.slice.util.Add;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ImageAnalysisController {
     private Logger LOGGER = LoggerFactory.getLogger(ImageAnalysisController.class);
+
     @ResponseBody
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    String add(@RequestBody Add param) {
-        Double a = Double.valueOf(param.getA());
-        Double b = Double.valueOf(param.getB());
-        LOGGER.info("requst add service: a:{},b:{}",a,b);
-        return String.format("%f", a + b);
+    @RequestMapping(value = "/add")
+    Double add(@RequestParam Double a, @RequestParam Double b) {
+        LOGGER.info("requst add service: a:{},b:{}", a, b);
+        return a + b;
     }
 }
